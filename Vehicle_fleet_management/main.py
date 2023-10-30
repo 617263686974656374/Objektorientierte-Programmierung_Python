e81286ac4866a787252d245ef396b750027b85c2
Jaroslav <122451815+617263686974656374@users.noreply.github.com>
2023-10-30 20:56:05 +0100

    Create main.py

---
+++ Vehicle_fleet_management/main.py
@@ -0,0 +1,11 @@
+from PyQt5 import QtWidgets
+from mainWindow_c import MainWindow
+
+
+if __name__ == "__main__":
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
